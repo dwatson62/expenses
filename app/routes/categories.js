d6d7bf51c84ed08fbf24929649c239d16f28a54f
Daryl Watson <darylwtsn@gmail.com>
2016-01-29 22:58:03 +0000

    WIP getting api to return items in category

--- app/routes/categories.js
+++ app/routes/categories.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var mongoose = require('mongoose');
 var Category = require('../models/category.js');
+var Item = require('../models/item.js');
 
 router.post('/category', function(req, res, next) {
   Category.create(req.body, function(err, category) {
@@ -20,9 +21,26 @@ router.get('/category/:id', function(req, res, next) {
   });
 });
 
+router.get('/category/:id/items', function(req, res,next) {
+  Category.findById(req.params.id, function(err, category) {
+    if (err) { return next(err); }
+    var data = [];
+    for (var i = 0; i < category.items.length; i ++) {
+      Item.findById(category.items[i], function(err, item) {
+        if (err) { return next(err); }
+        data.push({ 'item': item });
+        console.log(data)
+      });
+    }
+    console.log('Final data is: ' + data)
+    res.json({ item: data });
+  });
+});
+
 router.get('/categories', function(req, res) {
   Category.find(function(err, categories) {
     if (err) { return next(err); }
+    console.log(categories)
     res.json({ categories });
   });
 });
