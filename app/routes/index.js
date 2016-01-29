460d2879891aac25f4cf5be0555f6849f72b0277
Daryl Watson <darylwtsn@gmail.com>
2016-01-29 10:56:24 +0000

    API for category model setup

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,41 @@
 var express = require('express');
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router();
 
-// ROUTES FOR OUR API
-// =============================================================================
+var mongoose = require('mongoose');
+var Category = require('../models/category.js')
 
 router.get('/', function(req, res, next) {
   res.json({ 'message': 'Hello world' });
+});
+
+router.post('/category', function(req, res, next) {
+  Category.create(req.body, function(err, category) {
+    if (err) { return next(err); }
+    category.name = req.body.name;
+    console.log(category.name + ' was created!');
+    res.json(category)
+  });
+});
+
+router.get('/category/:id', function(req, res, next) {
+  Category.findById(req.params.id, function(err, category) {
+    if (err) { return next(err); }
+    res.json(category);
+  });
+});
+
+router.get('/categories', function(req, res) {
+  Category.find(function(err, categories) {
+    if (err) { return next(err); }
+    res.json({ categories });
+  })
+});
+
+router.delete('/category/:id', function(req, res, next) {
+  Category.findByIdAndRemove(req.params.id, function(err, category) {
+    if (err) { return next(err); }
+    res.json({ message: category.name + ' deleted!' });
+  });
 });
 
 module.exports = router;
